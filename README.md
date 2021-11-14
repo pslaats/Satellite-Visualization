94610786d1ea05d33cbfc753670b47f9156e79f0
Peter Slaats <peter.slaats@c3.ai>
2021-11-14 15:04:37 -0800

    Added a hook to use D3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Satellite-Visualization
 CS 519 Final Project
+
+# Deploy
+
+```
+$ cd path/to/Satellite-Visualization
+$ npm start
+$ cd client
+$ npm run start
+```
--- client/src/DeathStar.js
+++ client/src/DeathStar.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import * as d3 from 'd3';
+import React from "react";
+import * as d3 from "d3";
+import { useD3 } from "./hooks/useD3";
 
-function DeathStar() {
-  console.log(d3)
+function DeathStar({ data }) {
+  const ref = useD3((svg) => {
+    const height = 1000;
+    const width = 1000;
+    const margin = { top: 10, right: 20, bottom: 10, left: 20 };
+  });
   return (
-    <div>
-      I am the death star
-    </div>
-  )
+    <svg
+      ref={ref}
+      style={{
+        height: 1000,
+        style: "100%",
+        merginRight: "0px",
+        merginLeft: "0px",
+      }}
+    >
+      <g className="satellite-area" />
+    </svg>
+  );
 }
 
-export default DeathStar
+export default DeathStar;
