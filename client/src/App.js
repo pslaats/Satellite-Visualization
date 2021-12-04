4bf9c088989e7f77b0cf9ba93babb9dad56279ce
Peter Slaats <peter.slaats@c3.ai>
2021-12-04 15:26:21 -0800

    Globe renders again checkpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import DeathStar from "./DeathStar";
 import satellitesTLERaw from "./data/satellites.txt";
+import geographyData from "./data/countries_geo_json";
 import { parseRawTleData } from "./utils/satelliteHelpers";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState(null);
-  const [satelliteData, setSatelliteData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,8 +14,10 @@ function App() {
       const rawData = await res.text();
       const parsedData = parseRawTleData(rawData);
 
-      setData(parsedData[0].id);
-      setSatelliteData(parseRawTleData(rawData));
+      if (parsedData.length) {
+        setData(parsedData[0].id);
+        setSatelliteData(parseRawTleData(rawData));
+      }
     };
     fetchData();
   }, [data]);
@@ -23,8 +25,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-        <DeathStar data={data} />
+        {geographyData ? (
+          <DeathStar data={data} geography={geographyData} />
+        ) : null}
       </header>
     </div>
   );
--- client/src/DeathStar.js
+++ client/src/DeathStar.js
@@ -1,40 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
-import { feature } from "topojson-client";
 
 function DeathStar(props) {
-  const { data } = props;
-  const [geographies, setGeographies] = useState([]);
-  const [rotation, setRotation] = useState(0);
+  const { data, geography } = props;
+  const globeRef = useRef();
+  const componentRef = useRef();
 
   useEffect(() => {
-    d3.json("https://unpkg.com/world-atlas@2.0.2/countries-50m.json").then(
-      (response) => {
-        setGeographies(feature(response, response.objects.countries).features);
-      }
-    );
-  }, []);
-
-  const projection = d3
-    .geoOrthographic()
-    .scale(250)
-    .clipAngle(90)
-    .rotate(rotation);
+    const globe = d3.select(globeRef.current);
+    // const { width , height } = componentRef.current.getBoundingClientRect();
+    const projection = d3.geoOrthographic();
+    const pathGen = d3.geoPath().projection(projection);
+    console.log(geography.features)
+    globe
+      .selectAll('.country')
+      .data(geography.features)
+      .join('path')
+      .attr('class', 'country')
+      .attr('d', country => pathGen(country));
+  }, [geography]);
 
   return (
-    <svg width={1000} height={1000}>
-      <g className="countries">
-        {geographies.map((d, i) => (
-          <path
-            key={`country-${i}`}
-            d={d3.geoPath().projection(projection)(d)}
-            className="country"
-            stroke="#FFFFFF"
-            strokeWidth={1}
-          />
-        ))}
-      </g>
-    </svg>
+    <div ref={componentRef} style={{ width: '1000px', height: '1000px' }}>
+      <svg ref={globeRef} style={{ width: '1000px', height: '1000px' }}>
+      </svg>
+    </div>
   );
 }
 
