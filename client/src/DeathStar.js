98fcfe411d1daafff882a091444f79a1c09d6a52
Peter Slaats <peter.slaats@c3.ai>
2021-12-05 14:17:08 -0800

    Globe rotates fine enough, will test out with Armaan

--- client/src/DeathStar.js
+++ client/src/DeathStar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 
+// Rotation constants
 const ROTATION_SENSITIVITY = 300;
 const TIMER_SENSITIVITY = 1000;
 
@@ -33,14 +34,22 @@ function DeathStar(props) {
       .style("fill", "#777")
       .style("stroke", "#555");
 
-    // Rotate the globe
-    const timer = d3.timer(function (elapsed) {
-      const rotate = projection.rotate();
+    // Rotate function
+    const rotateOnTimer = (elapsed) => {
+      const currentRotation = projection.rotate();
       const k = ROTATION_SENSITIVITY / projection.scale();
-      projection.rotate([rotate[0] + k, rotate[1]]);
+      projection.rotate([currentRotation[0] + k, currentRotation[1]]);
       const pathRotate = d3.geoPath().projection(projection);
       globe.selectAll("path").attr("d", pathRotate);
-    }, TIMER_SENSITIVITY);
+    }
+
+    // Rotate the globe
+    const timer = d3.timer(rotateOnTimer, TIMER_SENSITIVITY);
+
+    // Add mouse over and mouse out actions
+    globe
+      .on('mouseover', (d) => { console.log('mouseover'); timer.stop(); } )
+      .on('mouseleave', (d) => { console.log('mouseout'); timer.restart(rotateOnTimer, TIMER_SENSITIVITY); } )
 
   }, [geography]);
 
