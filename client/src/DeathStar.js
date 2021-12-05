c238ab116b1d020b54682aaf31e6426074b28cef
Peter Slaats <peter.slaats@c3.ai>
2021-12-04 16:27:00 -0800

    timer rotates the globe but behavior is unstable

--- client/src/DeathStar.js
+++ client/src/DeathStar.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 
+const ROTATION_SENSITIVITY = 300;
+const TIMER_SENSITIVITY = 1000;
+
 function DeathStar(props) {
   const { data, geography } = props;
   const globeRef = useRef();
@@ -8,22 +11,42 @@ function DeathStar(props) {
 
   useEffect(() => {
     const globe = d3.select(globeRef.current);
-    // const { width , height } = componentRef.current.getBoundingClientRect();
+    const { width, height } = componentRef.current.getBoundingClientRect();
     const projection = d3.geoOrthographic();
-    const pathGen = d3.geoPath().projection(projection);
-    console.log(geography.features)
+    const path = d3.geoPath().projection(projection);
+
+    // Set height and width of svg to parent div
     globe
-      .selectAll('.country')
+      .style("width", width)
+      .style("height", height);
+
+    // Add country data
+    globe
+      .selectAll(".country")
       .data(geography.features)
-      .join('path')
-      .attr('class', 'country')
-      .attr('d', country => pathGen(country));
+      .join("path")
+      .attr("class", "country")
+      .attr("d", (country) => path(country));
+
+    // Style the globe to look nicer
+    globe
+      .style("fill", "#777")
+      .style("stroke", "#555");
+
+    // Rotate the globe
+    const timer = d3.timer(function (elapsed) {
+      const rotate = projection.rotate();
+      const k = ROTATION_SENSITIVITY / projection.scale();
+      projection.rotate([rotate[0] + k, rotate[1]]);
+      const pathRotate = d3.geoPath().projection(projection);
+      globe.selectAll("path").attr("d", pathRotate);
+    }, TIMER_SENSITIVITY);
+
   }, [geography]);
 
   return (
-    <div ref={componentRef} style={{ width: '1000px', height: '1000px' }}>
-      <svg ref={globeRef} style={{ width: '1000px', height: '1000px' }}>
-      </svg>
+    <div ref={componentRef} className="DeathStar">
+      <svg ref={globeRef}></svg>
     </div>
   );
 }
